7927c5cf12c320e025566fbb66ec024e9fa9c88b
AvramovDenis <avramov.91@mail.ru>
2020-03-05 00:35:55 +0300

    Выполнил задание 2 и 3

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,15 +1,110 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace Task2
 {
+    //Аврамов Д.В.
+
+    //2. Модифицировать программу нахождения минимума функции так, чтобы можно было передавать функцию в виде делегата.
+    //а) Сделайте меню с различными функциями и предоставьте пользователю выбор, для какой функции и на каком отрезке находить минимум.
+    //б) Используйте массив(или список) делегатов, в котором хранятся различные функции.
+    //в) * Переделайте функцию Load, чтобы она возвращала массив считанных значений. Пусть она возвращает минимум через параметр.
+
     class Program
     {
+        public delegate double MyFunction(double x);
+
         static void Main(string[] args)
         {
+            MyFunction[] Functions = { F1, F2, F3, F4 };
+
+            Console.WriteLine("Выбирите одну из функций: ");
+            Console.WriteLine("1: x * x - 50 * x + 10");
+            Console.WriteLine("2: 2 * x");
+            Console.WriteLine("3: x * x * x");
+            Console.WriteLine("4: x * x * x * x");
+
+            int f = Int32.Parse(Console.ReadLine()) - 1;
+
+            Console.WriteLine("Введите начало отрезка: ");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите конец отрезка: ");
+            double b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите шаг: ");
+            double h = double.Parse(Console.ReadLine());
+
+            SaveFunc("data.bin", Functions[f], a, b, h);
+
+            double min;
+
+            double[] d = Load("data.bin", out min);
+
+            Console.WriteLine("Результат: ");
+            
+            for (int i = 0; i < d.Length; i++)
+            {
+                Console.WriteLine(d[i]);
+            }
+
+            Console.WriteLine($"Minimum = {min}");
+
+            Console.ReadKey();
+
         }
+                
+                
+        public static double F1(double x)
+        {
+            return x * x - 50 * x + 10;
+        }
+
+        public static double F2 (double x)
+        {
+            return 2 * x;
+        }
+
+        public static double F3(double x)
+        {
+            return x * x * x;
+        }
+
+        public static double F4(double x)
+        {
+            return x * x * x * x;
+        }
+
+        public static void SaveFunc(string fileName, MyFunction f, double a, double b, double h)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+            BinaryWriter bw = new BinaryWriter(fs);
+            double x = a;
+            while (x <= b)
+            {
+                bw.Write(f(x));
+                x += h;
+            }
+            bw.Close();
+            fs.Close();
+        }
+
+        public static double[] Load(string fileName, out double min)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            BinaryReader bw = new BinaryReader(fs);
+            min = double.MaxValue;
+            double[] d = new double[fs.Length / sizeof(double)];
+            for (int i = 0; i < fs.Length / sizeof(double); i++)
+            {
+                d[i] = bw.ReadDouble();
+                if (d[i] < min) min = d[i];
+            }
+            bw.Close();
+            fs.Close();
+            return d;
+        }
+
     }
 }
---
+++ Task3/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    //Аврамов Д.В.
+
+    //Переделать программу Пример использования коллекций для решения следующих задач:
+    //а) Подсчитать количество студентов учащихся на 5 и 6 курсах;
+    //б) подсчитать сколько студентов в возрасте от 18 до 20 лет на каком курсе учатся(*частотный массив);
+    //в) отсортировать список по возрасту студента;
+    //г) * отсортировать список по курсу и возрасту студента;
+    //д) разработать единый метод подсчета количества студентов по различным параметрам
+    //выбора с помощью делегата и методов предикатов.
+
+    class Program
+    {
+        public static int MinAge = 18;
+        public static int MaxAge = 20;
+        public static int CurrentCourse = 1;
+
+        static void Main(string[] args)
+        {
+            string _file = "students_1.csv";
+
+            List<Student> list = new List<Student>();
+
+            StreamReader sr = new StreamReader(_file);
+
+            while (!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+
+                list.Add(new Student(line));
+                
+            }
+
+            sr.Close();
+
+            Console.WriteLine($"Общее количество студентов: {list.Count}");
+
+            Console.WriteLine($"Количество студентов от 18 до 20 лет : {list.FindAll(AgeFilter).Count}");
+
+            CurrentCourse = 5;
+            Console.WriteLine($"Количество студентов, учащихся на 5 курсе: {list.FindAll(CourseFilter).Count}");
+
+            CurrentCourse = 6;
+            Console.WriteLine($"Количество студентов, учащихся на 6 курсе: {list.FindAll(CourseFilter).Count}");
+
+            List<Student> listByAge = list.FindAll(AgeFilter);
+
+            int[] courseFriquency = new int[6];
+
+            for (int i = 0; i < 6; i++)
+            {
+                CurrentCourse = i + 1;
+                courseFriquency[i] = listByAge.FindAll(CourseFilter).Count;
+                Console.WriteLine($"Количество студентов от 18 до 20 лет на {i + 1} курсе: {courseFriquency[i]}");
+            }
+
+            list.Sort(MyComporisonByAge);//сортировка по возрасту
+                                          
+            list.Sort(MyComporisonByCourseAndAge);//сортировка по курсу и возрасту (сортировка по курсу, в пределах каждого курса по возрасту)
+                        
+            Console.ReadKey();
+        }
+
+        public static bool AgeFilter(Student s) { return s.Age >= MinAge && s.Age <= MaxAge; }
+
+        public static bool CourseFilter(Student s) { return s.Course == CurrentCourse; }
+
+        public static int MyComporisonByAge(Student a, Student b) { return a.Age.CompareTo(b.Age); }
+
+        public static int MyComporisonByCourse(Student a, Student b) { return a.Course.CompareTo(b.Course); }
+
+        public static int MyComporisonByCourseAndAge(Student a, Student b)
+        {
+            int course = a.Course.CompareTo(b.Course);
+
+            int age = a.Age.CompareTo(b.Age);
+
+            if (course == 0)
+            {
+                return age;
+            }
+            else
+            {
+                return course;
+            }
+                        
+        }
+                       
+    }
+
+    public class Student
+    {
+        public string FirstName;
+        public string SecondName;
+        public string Univercity;
+        public string Faculty;
+        public string Department;
+        public int Age;
+        public int Course;
+        public int Group;
+        public string City;
+
+        public Student(string line)
+        {
+            string[] values = line.Split(';');
+            FirstName = values[0];
+            SecondName = values[1];
+            Univercity = values[2];
+            Faculty = values[3];
+            Department = values[4];
+            Age = Int32.Parse(values[5]);
+            Course = Int32.Parse(values[6]);
+            Group = Int32.Parse(values[7]);
+            City = values[8];
+        }
+
+    }
+
+
+}
